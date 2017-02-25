43740ba88acc1b8897c8dcb1fa5662b7515426b4
AppHunterFood <tccappmetodista@gmail.com>
2017-02-25 19:27:52 -0300

    Criando Campos de Filtro (Preco e Categoria)

--- app/src/main/java/com/example/appof/TelaprincipalActivity.java
+++ app/src/main/java/com/example/appof/TelaprincipalActivity.java
@@ -5,9 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.LoginFilter;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.security.auth.login.LoginException;
 
 public class TelaprincipalActivity extends AppCompatActivity {
 
@@ -18,16 +26,27 @@ public class TelaprincipalActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Hunter Food");
         setSupportActionBar(toolbar);
+        //Populando Array para colocar na Spinner
+        //Inicio
+        Spinner s_preco = (Spinner) findViewById(R.id.s_preco);
+        List<String> preco = new ArrayList<>();
+            preco.add("20 reais");
+            preco.add("25 reais");
+            preco.add("30 reais");
+        ArrayAdapter<String> adapterSpinner = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, preco);
+        s_preco.setAdapter(adapterSpinner);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Spinner s_categoria = (Spinner) findViewById(R.id.s_categoria);
+        List<String> categoria = new ArrayList<>();
+            categoria.add("Lanche");
+            categoria.add("Prato Feito");
+            categoria.add("Doçes");
+        ArrayAdapter<String> adapterSpinner2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, categoria);
+        s_categoria.setAdapter(adapterSpinner2);
+        //Fim
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
 
