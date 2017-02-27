4f829404698e16fb2f4ed26f9562c8740516a0a2
AppHunterFood <tccappmetodista@gmail.com>
2017-02-27 18:02:28 -0300

    Quando clicar no menu chamar as respectivas telas

---
+++ app/src/main/java/com/example/appof/LocaisActivity.java
@@ -0,0 +1,13 @@
+package com.example.appof;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocaisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_meuslocais);
+    }
+}
--- app/src/main/java/com/example/appof/TelaprincipalActivity.java
+++ app/src/main/java/com/example/appof/TelaprincipalActivity.java
@@ -1,5 +1,8 @@
 package com.example.appof;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,12 +15,23 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.security.auth.login.LoginException;
 
 public class TelaprincipalActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,39 +44,50 @@ public class TelaprincipalActivity extends AppCompatActivity {
         //Inicio
         Spinner s_preco = (Spinner) findViewById(R.id.s_preco);
         List<String> preco = new ArrayList<>();
-            preco.add("20 reais");
-            preco.add("25 reais");
-            preco.add("30 reais");
+        preco.add("20 reais");
+        preco.add("25 reais");
+        preco.add("30 reais");
         ArrayAdapter<String> adapterSpinner = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, preco);
         s_preco.setAdapter(adapterSpinner);
 
         Spinner s_categoria = (Spinner) findViewById(R.id.s_categoria);
         List<String> categoria = new ArrayList<>();
-            categoria.add("Lanche");
-            categoria.add("Prato Feito");
-            categoria.add("Doçes");
+        categoria.add("Lanche");
+        categoria.add("Prato Feito");
+        categoria.add("Doçes");
         ArrayAdapter<String> adapterSpinner2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, categoria);
         s_categoria.setAdapter(adapterSpinner2);
         //Fim
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
 
         int id = item.getItemId();
 
-        if(id == R.id.locais){
+        if (id == R.id.locais) {
+
+            Intent menulocais = new Intent(this, LocaisActivity.class);
+            startActivity(menulocais);
+
             return true;
-        }
-        else if(id == R.id.configuracao){
+
+        } else if (id == R.id.configuracao) {
+
+            Intent configuracao = new Intent(this, ConfiguracaoActivity.class);
+            startActivity(configuracao);
+
             return true;
         }
         return super.onContextItemSelected(item);
@@ -70,4 +95,39 @@ public class TelaprincipalActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Telaprincipal Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
